fbe5a3bcd48c382f5d6be9c3d27fe3cb96ba5709
Ed Gibbs <edward_gibbs@yahoo.com>
2017-09-30 18:54:07 -0700

    Dump out game state on web page

--- lib/gallows_web/views/hangman_view.ex
+++ lib/gallows_web/views/hangman_view.ex
@@ -1,3 +1,14 @@
 defmodule GallowsWeb.HangmanView do
   use GallowsWeb, :view
+
+  def game_state(state) do
+  end
+
+  defp alert(class, message) do
+    """
+    <div class="alert alert-#{class}">
+    #{message}
+    </div>
+    """
+  end
 end
