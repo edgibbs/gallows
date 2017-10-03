c64850d24d45a66d3a1fb3ef2eb7ea6aebddfc6b
Ed Gibbs <edward_gibbs@yahoo.com>
2017-10-02 21:23:34 -0700

    Make a nicer view

--- lib/gallows_web/views/hangman_view.ex
+++ lib/gallows_web/views/hangman_view.ex
@@ -1,14 +1,12 @@
 defmodule GallowsWeb.HangmanView do
   use GallowsWeb, :view
+  import GallowsWeb.Views.Helpers.GameStateHelper
 
-  def game_state(state) do
+  def game_over?(%{ game_state: game_state }) do
+    game_state in [ :won, :lost ]
   end
 
-  defp alert(class, message) do
-    """
-    <div class="alert alert-#{class}">
-    #{message}
-    </div>
-    """
+  def new_game_button(conn) do
+    button("New Game", to: hangman_path(conn, :create_game) )
   end
 end
